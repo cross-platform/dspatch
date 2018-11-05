3ceaa5b0dc307a4c2bce398b27a8109e0ac04395
Marcus Tomlinson <themarcustomlinson@gmail.com>
2018-11-05 10:18:53 +0000

    Fix Windows build

--- tests/components/SlowCounter.h
+++ tests/components/SlowCounter.h
@@ -22,7 +22,7 @@ public:
 protected:
     virtual void Process_( SignalBus const&, SignalBus& outputs ) override
     {
-        std::this_thread::sleep_for( std::chrono::nanoseconds( 1 ) );
+        std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
 
         outputs.SetValue( 0, _count++ );
     }
