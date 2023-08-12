3b7f5e15b69dca83bda471d73150c0fcb44f8ca5
Marcus Tomlinson <themarcustomlinson@gmail.com>
2023-08-12 14:55:00 +0100

    Exclude subprojects from docs

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['any_0',['any',['../classfast__any_1_1any.html',1,'fast_any']]],
-  ['autotickthread_1',['AutoTickThread',['../class_d_s_patch_1_1internal_1_1_auto_tick_thread.html',1,'DSPatch::internal']]]
+  ['autotickthread_0',['AutoTickThread',['../class_d_s_patch_1_1internal_1_1_auto_tick_thread.html',1,'DSPatch::internal']]]
 ];
