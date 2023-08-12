756511798c1f76839bbd7bb3586beb11720147e5
Marcus Tomlinson <themarcustomlinson@gmail.com>
2023-08-12 14:45:25 +0100

    9.5.2

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['autotickthread_0',['AutoTickThread',['../class_d_s_patch_1_1internal_1_1_auto_tick_thread.html',1,'DSPatch::internal']]]
+  ['any_0',['any',['../classfast__any_1_1any.html',1,'fast_any']]],
+  ['autotickthread_1',['AutoTickThread',['../class_d_s_patch_1_1internal_1_1_auto_tick_thread.html',1,'DSPatch::internal']]]
 ];
