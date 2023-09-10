f62796196ed0f7142496951d465bbf7bf31e8eb0
Marcus Tomlinson <themarcustomlinson@gmail.com>
2023-09-10 14:32:17 +0100

    Clean up includes such that "src" is not needed

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -28,8 +28,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <dspatch/Circuit.h>
 
-#include <internal/AutoTickThread.h>
-#include <internal/CircuitThread.h>
+#include "internal/AutoTickThread.h"
+#include "internal/CircuitThread.h"
 
 #include <algorithm>
 #include <set>
