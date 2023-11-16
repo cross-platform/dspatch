2e23ad34ee90e341c78e3680ee9f18ad151016a8
Marcus Tomlinson <themarcustomlinson@gmail.com>
2023-11-16 14:06:08 +0000

    Fix bash script path evaluation

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR=$(cd `dirname $0` && pwd)
-cd $DIR/..
+DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd "$DIR/.."
 
 find . -iname *.h -o -iname *.cpp | grep -v build | grep -v builddir | grep -v subprojects | xargs clang-format --style=file --verbose -i
